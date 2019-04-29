b014bafe1018d87f538ae98829558918425ceca3
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-04-29 11:30:55 +0200

    Ändrade lite i texten

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -68,7 +68,7 @@ class PasswordForgetFormBase extends Component {
 
 const PasswordForgetLink = () => (
   <p>
-    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+    <Link to={ROUTES.PASSWORD_FORGET}>Have you forgot your password?</Link>
   </p>
 );
 
